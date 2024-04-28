6f88c8bcd40128ad30168d8cab010920e0f24404
satansin123 <sanchaynts9422@gmail.com>
2024-04-29 00:57:57 +0530

    contact us component

--- src/app/(general)/page.js
+++ src/app/(general)/page.js
@@ -17,6 +17,8 @@ import { collection } from "firebase/firestore";
 import { db } from "@/app/firebase";
 import { useCollection } from "react-firebase-hooks/firestore" ;
 
+import ContactUs from "@/components/ContactUs";
+
 export default function Home() {
 	const router = useRouter();
 	const [user, authLoading, authError] = useAuthState(auth);
@@ -168,38 +170,8 @@ export default function Home() {
 					}
 				</div>
 			}
-
-			<div className="flex flex-col items-center gap-[4px]">
-				<p className="text-[#159EEC] font-bold text-xl">GET IN TOUCH</p>
-				<p className="text-[#1F2B6C] font-bold text-3xl">CONTACT</p>
-			</div>
-
-			<div className="grid grid-cols-4 gap-[2em] m-[8em]">
-				<TextCard2Liner
-					src="/contact.svg"
-					title="EMERGENCY"
-					line1="(237) 681-812-255"
-					line2="(237) 681-812-255"
-				/>
-				<TextCard2Liner
-					src="/location.svg"
-					title="LOCATION"
-					line1="0123 Some place"
-					line2="9876 Some country"
-				/>
-				<TextCard2Liner
-					src="/email.svg"
-					title="EMAIL"
-					line1="fildineeesoe@ gmil.com"
-					line2="myebstudios@ gmail.com"
-				/>
-				<TextCard2Liner
-					src="/clock.svg"
-					title="WORKING HOURS"
-					line1="Mon-Sat 09:00-20:00"
-					line2="Sunday Emergency only"
-				/>
-			</div>
+			<ContactUs/>
+			
 		</div>);
 	}
 }
--- src/app/(general)/researchareas/page.js
+++ src/app/(general)/researchareas/page.js
@@ -5,8 +5,9 @@ import { db } from "@/app/firebase";
 import { useCollection } from "react-firebase-hooks/firestore" ;
 import Image from "next/image";
 import TwoDataDisplay from "@/components/TwoDataDisplay";
-import TextCard2Liner from "../../../components/TextCard2Liner";
+
 import RecentPosts from "../../../components/RecentPosts";
+import ContactUs from "@/components/ContactUs";
 export default function Blogs() {
 	const [researchAreasValue, researchAreasLoading, researchAreasError] = useCollection(collection(db, "researchareas"));
     const [blogsValue, blogsLoading, blogsError] = useCollection(collection(db, "blogs"));
@@ -59,37 +60,7 @@ export default function Blogs() {
       </div>
 		  </div>
 		  </div>
-		  <div className="flex flex-col items-center gap-[4px]">
-				<p className="text-[#159EEC] font-bold text-xl">GET IN TOUCH</p>
-				<p className="text-[#1F2B6C] font-bold text-3xl">CONTACT</p>
-			</div>
-
-			<div className="grid grid-cols-4 gap-[2em] m-[8em]">
-				<TextCard2Liner
-					src="/contact.svg"
-					title="EMERGENCY"
-					line1="(237) 681-812-255"
-					line2="(237) 681-812-255"
-				/>
-				<TextCard2Liner
-					src="/location.svg"
-					title="LOCATION"
-					line1="0123 Some place"
-					line2="9876 Some country"
-				/>
-				<TextCard2Liner
-					src="/email.svg"
-					title="EMAIL"
-					line1="fildineeesoe@ gmil.com"
-					line2="myebstudios@ gmail.com"
-				/>
-				<TextCard2Liner
-					src="/clock.svg"
-					title="WORKING HOURS"
-					line1="Mon-Sat 09:00-20:00"
-					line2="Sunday Emergency only"
-				/>
-			</div>
+		  <ContactUs/>
 		</div>
 	  );
 	}
